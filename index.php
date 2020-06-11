6906b6f9aae284f95af1dcd953a934b6f26d2604
Alisson de Oliveira <aalissonrafael@yahoo.com>
2020-06-11 18:30:20 -0300

    Iniciando o Back-End

--- index.php
+++ index.php
@@ -1,28 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt_br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Assistant:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <script src="https://kit.fontawesome.com/463d1c04d0.js" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="img/favicon.ico">
-    <title>Faça Login</title>
-</head>
-<body>
-    
-<div class="box_login">
-    <h1>FAÇA LOGIN!</h1>
+<?php
 
-    <form>
-        <input type="text" name="e-mail" placeholder="SEU E-MAIL">
-        <input type="password" name="password" placeholder="SUA SENHA">
+    require once 'app/core/core.php';
 
-        <button class="btn-login">LOGAR</button>
-        <span class="msg_error"><i class="fas fa-window-close"></i>Tentativa Inválida!</span>
-    </form>
+    $core = new Core;
+    $core->start($_GET);
 
-</div>
-
-</body>
-</html>
+    ?>

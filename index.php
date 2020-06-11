379ce0535e739b0da32a78ab9c4769b8258921ed
Alisson de Oliveira <aalissonrafael@yahoo.com>
2020-06-11 17:14:44 -0300

    Mudanças no index

--- index.php
+++ index.php
@@ -3,12 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="img/favicon.ico">
     <title>Faça Login</title>
 </head>
 <body>
     
 <div class="box_login">
+    <h1>FAÇA LOGIN!</h1>
 
+    <form>
+        <input type="text" name="e-mail" placeholder="SEU E-MAIL">
+        <input type="password" name="password" placeholder="SUA SENHA">
+
+        <button class="btn-login">LOGAR</button>
+    </form>
 
 </div>
 

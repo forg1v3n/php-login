97971c8f7c2bc821b060b008c7e5d29079e020bd
Alisson de Oliveira <aalissonrafael@yahoo.com>
2020-06-11 17:55:24 -0300

    Index finalizado

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Assistant:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/463d1c04d0.js" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="img/favicon.ico">
     <title>Faça Login</title>
 </head>
@@ -18,6 +19,7 @@
         <input type="password" name="password" placeholder="SUA SENHA">
 
         <button class="btn-login">LOGAR</button>
+        <span class="msg_error"><i class="fas fa-window-close"></i>Tentativa Inválida!</span>
     </form>
 
 </div>
